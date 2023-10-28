bb4d2d57568bb7de543da3e1c1ca05ba231d5295
alexander.nedorezov <alserned@gmail.com>
2023-10-28 15:13:18 +0700

    03-ter-02: task4

--- 03-ter-02-basics/src/outputs.tf
+++ 03-ter-02-basics/src/outputs.tf
@@ -1,7 +1,7 @@
 output "vm_external_ip" {
   description = "External IP address of instances"
   value       = {
-    "web" = yandex_compute_instance.platform.network_interface.0.nat_ip_address
-    "db" = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
+    (yandex_compute_instance.platform.name) = yandex_compute_instance.platform.network_interface.0.nat_ip_address
+    (yandex_compute_instance.platform-db.name) = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
   }
 }
